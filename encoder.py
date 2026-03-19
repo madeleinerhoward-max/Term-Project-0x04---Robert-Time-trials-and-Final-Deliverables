4f515861de3a53a6fe1c0a30a9af06d23ec52554
madeleinerhoward-max <madeleinerhoward@gmail.com>
2026-03-19 12:01:47 -0700

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,4 +1,3 @@
-# encoder.py  (REPLACE ENTIRE FILE)
 
 from pyb import Timer, Pin
 from utime import ticks_us, ticks_diff
@@ -9,26 +8,18 @@ class encoder:
 
     def __init__(self, tim, chA_pin, chB_pin):
         self.tim = tim
-
-        # Force pins into input mode with pull-ups (IMPORTANT for many encoders)
         self.pinA = Pin(chA_pin, mode=Pin.IN, pull=Pin.PULL_UP)
         self.pinB = Pin(chB_pin, mode=Pin.IN, pull=Pin.PULL_UP)
-
-        # Configure timer channels for encoder mode
         self.ch1 = self.tim.channel(1, pin=self.pinA, mode=Timer.ENC_AB)
         self.ch2 = self.tim.channel(2, pin=self.pinB, mode=Timer.ENC_AB)
-
-        # Optional: reset counter so you can see it move from 0
         self.tim.counter(0)
 
-        # Wrap handling
         try:
             self.AR = int(self.tim.period())
         except Exception:
             self.AR = 0xFFFF
         self.half_AR = (self.AR + 1) // 2
 
-        # State
         self.position = 0
         self.delta = 0
         self.dt = 1
@@ -45,7 +36,6 @@ class encoder:
         count = int(self.tim.counter())
         raw_delta = count - self.prev_count
 
-        # Wrap correction
         if raw_delta > self.half_AR:
             raw_delta -= (self.AR + 1)
         elif raw_delta < -self.half_AR:
@@ -68,6 +58,7 @@ class encoder:
         self.dt = 1
         self.prev_time = ticks_us()
         self.prev_count = int(self.tim.counter())
+
 
 
 
